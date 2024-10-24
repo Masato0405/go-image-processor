b9247fb9e7528a32091a28d44abe7c18fec670dc
masato <04masato05@gmail.com>
2024-10-24 23:40:58 +0900

    パラメータチェックとエラーを出すように, 関数名の修正

--- presigned-url/main.go
+++ presigned-url/main.go
@@ -12,14 +12,19 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	bucket := "your-s3-bucket"
-	key := "uploaded-image.jpg"
+func generatePresignedUrl(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	bucket := request.QueryStringParameters["bucket"]
+	key := request.QueryStringParameters["key"]
+
+	// バケット名 or 画像名が存在しない場合はエラー
+	if bucket == "" || key == "" {
+		return events.APIGatewayProxyResponse{StatusCode: 400, Body: "Missing bucket or key parameter"}, nil
+	}
 
 	// AWS Config作成 (v2)
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
 	if err != nil {
-		return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Failed to load configuration"}, err
+		return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Failed to load configuration" + err.Error()}, err
 	}
 
 	// S3クライアント作成
@@ -35,14 +40,15 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		opts.Expires = 15 * time.Minute
 	})
 
+	// 事前署名付きURLの生成失敗
 	if err != nil {
-		return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Failed to generate presigned URL"}, err
+		return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Failed to generate presigned URL" + err.Error()}, err
 	}
 
-	// URLをクライアントに返す
+	// 事前署名付きURLをクライアントに返す
 	return events.APIGatewayProxyResponse{StatusCode: 200, Body: fmt.Sprintf(`{"url": "%s"}`, presignResult.URL)}, nil
 }
 
 func main() {
-	lambda.Start(handler)
+	lambda.Start(generatePresignedUrl)
 }
